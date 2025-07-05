412155d2a61d8fd25820ecfffa0ec33b1f2a6a21
ranvviiimewada <ranvviiiiirajput07@gmail.com>
2025-07-05 10:34:26 +0530

    table

--- new.cpp
+++ new.cpp
@@ -1,10 +1,53 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//      int a;
+//      cout<<"Enter a Number: ";
+//      cin>>a;
+//      cout<<a;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//      int a;
+//      cout<<"Enter a Number: ";
+//      cin>>a;
+//      cout<<a;
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
 int main(){
 
-int a;
-cout<<"Enter a Number:";
-cin>>a;
-cout<<a;
+
+int a=10;
+cout<<a<<endl;
+
+float b=10.11;
+cout<<b<<endl;
+
+char c='$';
+cout<<c<<endl;
+
+bool d=true;
+cout<<d;
+
+
+
+//  int a;
+//  cout<<"Enter a Number";
+//  cin>>a;
+//  cout<<"The number you have entered is: "<<a;
+
+
 }
