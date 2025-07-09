4633fed86531fdc99b916ce23ad93c98c4b553d8
ranvviiimewada <ranvviiiiirajput07@gmail.com>
2025-07-09 06:11:46 +0530

    table 5

--- new.cpp
+++ new.cpp
@@ -27,20 +27,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+// int main() 
 
 
-int a=10;
-cout<<a<<endl;
+// int a=10;
+// cout<<a<<endl;
 
-float b=10.11;
-cout<<b<<endl;
+// float b=10.11;
+// cout<<b<<endl;
 
-char c='$';
-cout<<c<<endl;
+// char c='$';
+// cout<<c<<endl;
 
-bool d=true;
-cout<<d;
+// bool d=true;
+// cout<<d;
 
 
 
@@ -50,4 +50,156 @@ cout<<d;
 //  cout<<"The number you have entered is: "<<a;
 
 
+// }
+
+// int main (){
+
+// int a , b; 
+// cout<<"Enter a Number";
+// cin>>a;
+// cout<<"Enter a Number";
+// cin>>b;
+// cout<<a+b;
+
+// }
+
+
+int main (){
+
+// int side;
+// cout<<"Enter a Number";
+// cin>>side;
+// cout<<side*side;
+
+
+// area of rectangle
+
+// int b,h;
+// cout<<"enter base";
+// cin>>b;
+// cout<<"enter base";
+// cin>>h;
+// cout<<b*h;
+
+
+// are of circle
+
+// int r;
+// cout<<"enter a radius";
+// cin>> r;
+// cout<< 3.14*r*r;
+
+
+
+//    area of sqauare {
+//         int side;
+//         cout<<"Enter a side";
+//         cin>>side;
+//         cout<<side*side;
+//    }
+
+//    area of triangle 
+// {
+//         int b,h;
+//         cout<<"Enter a base" <<endl;
+//         cout<<"Enter a height"<< endl;
+//         cin>> b>>h;
+//         cout<<b*h/2;
+// }
+ 
+
+
+// int a=10;
+// int b=20;
+// cout<<a<<endl;
+// a=a+b;
+// cout<<a<<endl;
+// a-=b;
+// cout<<a<<endl;
+// a*=b;
+// cout<<a<<endl;
+// b/=a;
+// cout<<a<<endl;
+// b%=a;
+// cout<<a<<endl;
+// cout<<a;
+
+
+
+// int a=10;
+// int b=20;
+
+//  cout<<(a>b)<<endl;
+//  cout<<(a<b)<<endl;
+//  cout<<(a<=b)<<endl;
+//  cout<<(a>=b)<<endl;
+//  cout<<(a==b)<<endl;
+//  cout<<(a!=b)<<endl;
+
+
+// cout<<(1 && 0)<<endl;
+// cout<<(1 && 1)<<endl;
+// cout<<(05 && 07)<<endl;
+
+// cout<<(1 || 1)<<endl;
+// cout<<(1 && 1)<<endl;
+
+// cout<<(!10)<<endl;
+
+
+
+// int a=10;
+// int b=20;
+// int c=70;
+
+// cout<<( a>b && a>c )<<endl;
+// cout<<( b>a && b>c )<<endl;
+// cout<<( c>a && c>b )<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
